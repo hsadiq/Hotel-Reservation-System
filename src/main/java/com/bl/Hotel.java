f6fafcc5eeaf82ed914cff3f01994f8d04f30de4
hsadiq <ksadik320@gmail.com>
2023-07-17 16:21:44 +0530

    Added Find Cheapest Method In Hotel Reservation Systemmm

--- src/main/java/com/bl/Hotel.java
+++ src/main/java/com/bl/Hotel.java
@@ -1,11 +1,28 @@
 package com.bl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Hotel {
     private String Name;
     private  double RegularPrice;
-    public Hotel(String Name, double RegularPrice){
+
+    public double getPrice() {
+        return Price;
+    }
+
+    public void setPrice(double price) {
+        Price = price;
+    }
+
+    private double Price;
+
+    private Map<String, Double> prices;
+    public Hotel(String Name, double RegularPrice, double price){
         this.Name = Name;
+        this.Price = price;
         this.RegularPrice = RegularPrice;
+        this.prices = new HashMap<>();
     }
 
     public String getName() {
@@ -24,4 +41,11 @@ public class Hotel {
         RegularPrice = regularPrice;
     }
 
+    public void setPrices(String date, double price){
+        prices.put(date, price);
+    }
+
+    public double getPricesForDates(String date){
+        return prices.getOrDefault(date,0.0);
+    }
 }
--- src/main/java/com/bl/HotelReservation.java
+++ src/main/java/com/bl/HotelReservation.java
@@ -1,7 +1,6 @@
 package com.bl;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class HotelReservation {
@@ -15,18 +14,47 @@ public class HotelReservation {
         hotels.add(hotel);
     }
 
+    public Hotel findCheapestHotel(String date){
+        Hotel cheapestHotel = null;
+        double cheapestPrice = Double.MAX_VALUE;
+
+        for (Hotel hotel:hotels) {
+            double price = hotel.getPricesForDates(date);
+            if (price < cheapestPrice){
+                cheapestPrice = price;
+                cheapestHotel = hotel;
+            }
+        }
+        return cheapestHotel;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Hotel Reservation System");
 
         HotelReservation reservation = new HotelReservation();
 
-        Hotel hotel1 = new Hotel("Lakewood", 150);
-        Hotel hotel2 = new Hotel("BridgeWood", 200);
-        Hotel hotel3 = new Hotel("RidgeWood", 250);
+        Hotel hotel1 = new Hotel("Lakewood", 150, 200);
+        Hotel hotel2 = new Hotel("BridgeWood", 200, 250);
+        Hotel hotel3 = new Hotel("RidgeWood", 250, 300);
+
+
+        hotel1.setPrices("10-08-2020", 220);
+        hotel2.setPrices("11-08-2020", 300);
+        hotel3.setPrices("12-08-2020", 400);
 
         reservation.addHotel(hotel1);
         reservation.addHotel(hotel2);
         reservation.addHotel(hotel3);
+
+        String inputDate = "10-08-2020";
+
+        Hotel cheapestHotel = reservation.findCheapestHotel(inputDate);
+
+        if (cheapestHotel != null){
+            System.out.println("Cheapest Hotel for Date: " + inputDate + " is" + "\t" + cheapestHotel.getName());
+        }else {
+            System.out.println("NO Hotel Available for Date: " + inputDate);
+        }
 
     }
 }
